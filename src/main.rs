4ed42da7eb01be6e88e6d0320e7077a805a2842f
Vaclav Stebra <vstebra@gmail.com>
2017-07-02 21:24:08 +0200

    Methods on structs

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,36 @@ struct Rectangle {
     width: u32,
 }
 
+impl Rectangle {
+    fn area (&self) -> u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+
+    // associated function
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { length: 50, width: 30 };
+    let rect2 = Rectangle { length: 40, width: 10 };
+    let rect3 = Rectangle { length: 45, width: 60 };
+
+    let sq = Rectangle::square(20);
 
     println!("rect1 is {:?}", rect1);
+    println!("sq is {:?}", sq);
 
     println!(
         "The are of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.length * rectangle.width
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
